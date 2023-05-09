b3195591bfde30f60539acd5a46cda2cf6e0257a
Medeiros <hsymetry@gmail.com>
2023-05-09 19:11:18 -0300

    first commit

---
+++ lib/src/modules/home/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import '../../core/env/env.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Page'),
+      ),
+      body: Center(
+        child: Text(Env.instance.get('backend_base_url')),
+      ),
+    );
+  }
+}
