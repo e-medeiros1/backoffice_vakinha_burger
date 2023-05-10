3f26d994c35d70957dc70623ec4154c828e897cb
Medeiros <hsymetry@gmail.com>
2023-05-10 11:52:49 -0300

    Add Theme configuration

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'core/ui/theme/theme_config.dart';
+
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
   
@@ -9,7 +11,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Vakinha Burger',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeConfig.theme,
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
     );
