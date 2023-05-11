595c21c4d0d80b322f1407bbe077e554eeb1c73a
Medeiros <hsymetry@gmail.com>
2023-05-11 19:41:14 -0300

    Implement login

--- lib/src/modules/login/login_module.dart
+++ lib/src/modules/login/login_module.dart
@@ -1,12 +1,20 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../repositories/auth/auth_repository_impl.dart';
+import '../../services/auth/login_service_impl.dart';
+import 'login_controller.dart';
 import 'login_page.dart';
 
 class LoginModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.lazySingleton((i) => AuthRepositoryImpl(i())),
+        Bind.lazySingleton((i) => LoginServiceImpl(i(), i())),
+        Bind.lazySingleton((i) => LoginController(i())),
+      ];
 
   @override
-  List<ModularRoute> get routes =>
-      [ChildRoute('/', child: (context, args) => const LoginPage())];
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const LoginPage()),
+      ];
 }
--- lib/src/modules/login/login_page.dart
+++ lib/src/modules/login/login_page.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
+import 'package:validatorless/validatorless.dart';
 
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
 import '../../core/ui/helpers/size_extensions.dart';
 import '../../core/ui/styles/colors_app.dart';
 import '../../core/ui/styles/text_styles.dart';
+import 'login_controller.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> with Loader, Messages {
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  late final ReactionDisposer statusReactionDisposer;
+  final controller = Modular.get<LoginController>();
+
+  @override
+  void initState() {
+    statusReactionDisposer = reaction((_) => controller.loginStatus, (status) {
+      switch (status) {
+        case LoginStateStatus.initial:
+          break;
+        case LoginStateStatus.loading:
+          showLoader();
+          break;
+        case LoginStateStatus.success:
+          hideLoader();
+          Modular.to.navigate('/');
+          break;
+        case LoginStateStatus.error:
+          hideLoader();
+          showError(controller.errorMessage ?? 'Erro ao logar');
+          break;
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailEC.dispose();
+    passwordEC.dispose();
+    statusReactionDisposer();
+    super.dispose();
+  }
+
+  void _formSubmit() {
+    final isValid = formKey.currentState?.validate() ?? false;
+    if (isValid) {
+      controller.login(emailEC.text, passwordEC.text);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,80 +67,100 @@ class LoginPage extends StatelessWidget {
     final screenWidth = context.screenWidth;
     return Scaffold(
       backgroundColor: context.colors.black,
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: screenShortestSide * .5,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/lanche.png'),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: context.percentHeight(.10)),
-            height: screenShortestSide * .5,
-            child: Image.asset('assets/images/logo.png'),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              constraints: BoxConstraints(
-                maxWidth: context.percentWidth(screenWidth < 1300 ? .7 : .3),
-              ),
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      FractionallySizedBox(
-                        widthFactor: .3,
-                        child: Image.asset('assets/images/logo.png'),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Text('Login', style: context.textStyles.textButtonTitle),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(labelText: 'E-mail'),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        decoration: const InputDecoration(labelText: 'Senha'),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('Entrar'),
-                        ),
-                      )
-                    ],
+      body: Form(
+        key: formKey,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: screenShortestSide * .5,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/images/lanche.png'),
                   ),
                 ),
               ),
             ),
-          )
-        ],
+            Container(
+              padding: EdgeInsets.only(top: context.percentHeight(.10)),
+              height: screenShortestSide * .5,
+              child: Image.asset('assets/images/logo.png'),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                constraints: BoxConstraints(
+                  maxWidth: context.percentWidth(screenWidth < 1300 ? .7 : .3),
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        FractionallySizedBox(
+                          widthFactor: .3,
+                          child: Image.asset('assets/images/logo.png'),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          'Entrar',
+                          style: context.textStyles.textButtonTitle,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          controller: emailEC,
+                          onFieldSubmitted: (_) => _formSubmit(),
+                          decoration:
+                              const InputDecoration(labelText: 'E-mail'),
+                          validator: Validatorless.multiple([
+                            Validatorless.required('E-mail obrigatório'),
+                            Validatorless.email('E-mail inválido'),
+                          ]),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          controller: passwordEC,
+                          onFieldSubmitted: (_) => _formSubmit(),
+                          obscureText: true,
+                          decoration: const InputDecoration(labelText: 'Senha'),
+                          validator: Validatorless.multiple([
+                            Validatorless.required('Senha obrigatória'),
+                            Validatorless.min(6, 'Mínimo de 6 caracteres'),
+                          ]),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: _formSubmit,
+                            child: const Text('Entrar'),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
