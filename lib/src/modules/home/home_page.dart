8d0412c96f91f6d33efd0a6d7cc02c17c7eec016
Medeiros <hsymetry@gmail.com>
2023-05-11 22:04:02 -0300

    Add home page

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
 import '../../core/env/env.dart';
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import '../template/base_layout.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Main Page'),
-      ),
+    return BaseLayout(
       body: Center(
         child: Text(Env.instance.get('backend_base_url')),
       ),
