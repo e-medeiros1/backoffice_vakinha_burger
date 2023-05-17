f2ae388b8efb9986b4d4902e4e2c072541453917
Medeiros <hsymetry@gmail.com>
2023-05-17 14:10:13 -0300

    Add session storage

--- lib/src/core/storage/session_storage.dart
+++ lib/src/core/storage/session_storage.dart
@@ -1,5 +1,13 @@
-abstract class SessionStorage {
-  void setData(String key, String value);
-  String getData(String key);
-  void clean();
+import 'dart:html';
+import 'storage.dart';
+
+class SessionStorage extends Storage {
+  @override
+  String getData(String key) => window.sessionStorage[key] ?? '';
+
+  @override
+  void setData(String key, String value) => window.sessionStorage[key] = value;
+
+  @override
+  void clean() => window.sessionStorage.clear();
 }
